1feb40672d6c232b7c9375d897fca108b34d1430
p011uckz <mrbendy88@gmail.com>
2022-07-25 17:35:09 -0400

    started a form

--- frontend/marvel-wiki/src/pages/Home.jsx
+++ frontend/marvel-wiki/src/pages/Home.jsx
@@ -6,6 +6,9 @@ const Home = () => {
     <>
     This is a Home Page
     This is where you will sign in to your profile and see your saved stuff
+    <form>
+      
+    </form>
     
     </>
   )
